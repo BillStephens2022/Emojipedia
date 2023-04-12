3a85ff6cfb93dbfe7b066471809ccd524b8013ba
BillStephens2022 <two4onebill@yahoo.com>
2023-04-12 16:36:19 -0400

    restructured home page into its own component, updated React router routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Entry from "./Entry";
-import emojipedia from "../emojipedia";
+import Home from "./Home";
+
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+    <>
       <Routes>
-        <Route exact path="/">
-          <div>
-            <h1>
-              <span>emojipedia</span>
-            </h1>
-            <dl className="dictionary">
-              {emojipedia.map((emojiTerm) => (
-                <Entry
-                  key={emojiTerm.id}
-                  emoji={emojiTerm.emoji}
-                  name={emojiTerm.name}
-                  meaning={emojiTerm.meaning}
-                />
-              ))}
-            </dl>
-          </div>
-        </Route>
+        <Route path="/" element={<Home />} />
       </Routes>
+      </>
     </Router>
   );
 }
